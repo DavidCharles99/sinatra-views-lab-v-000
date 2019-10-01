5943328fde3fed3af0048a17cf660b9abf8b5389
David Charles <ratlover03@gmail.com>
2019-10-01 22:59:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ class App < Sinatra::Base
 		erb :index
 	end
 
-get '/hello' do
-	erb :hello
-end
+	get '/hello' do
+		erb :hello
+	end
 
-get '/goodbye' do
-	erb :goodbye
-end
+	get '/goodbye' do
+		erb :goodbye
+	end
 
-get '/date' do
-	erb :date
-end
+	get '/date' do
+		erb :date
+	end
 end
